28b6f39429da69b69ba0ae99dd2186145b0fb841
ambertide <egeemirozkan24@gmail.com>
2022-07-20 03:45:05 +0300

    feat: add better debugging

--- tests/check_cpu.c
+++ tests/check_cpu.c
@@ -2,10 +2,34 @@
 #include <json.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stdarg.h>
+
 #include <string.h>
 #include <libbeemu/device/processor.h>
 #include <libbeemu/device/memory.h>
 #include <libbeemu/device/registers.h>
+
+/**
+ * @brief Asser equal and print a formatted message.
+ *
+ */
+#define TEST_ASSERT_EQUAL_MESSAGEF(expected, actual, format, ...) \
+	{                                                             \
+		char msg[100];                                            \
+		snprintf(msg, 100, format, __VA_ARGS__);                  \
+		TEST_ASSERT_EQUAL_MESSAGE(expected, actual, msg);         \
+	}
+
+/**
+ * @brief Asser equal and print a formatted message.
+ *
+ */
+#define TEST_MESSAGEF(format, ...)               \
+	{                                            \
+		char msg[100];                           \
+		snprintf(msg, 100, format, __VA_ARGS__); \
+		TEST_MESSAGE(msg);                       \
+	}
 
 const int path_name_length = 48;
 typedef struct TestFiles
@@ -87,16 +111,17 @@ void exec_test_case(json_object *test)
 	json_object_object_foreach(initial_cpu_values, key, val)
 	{
 		const char *register_name = key;
+		const char *value = json_object_get_string(val);
 		if (strncmp(register_name, "sp", 2) == 0 || strncmp(register_name, "pc", 2) == 0)
 		{
 			const BeemuRegister_16 register_ = strncmp(register_name, "sp", 2) == 0 ? BEEMU_REGISTER_SP : BEEMU_REGISTER_PC;
-			beemu_registers_write_16(processor->registers, register_, (uint16_t)strtol(val, NULL, 0));
+			beemu_registers_write_16(processor->registers, register_, (uint16_t)strtol(value, NULL, 0));
 		}
 		else
 		{
 			// 8 bit registers
 			const BeemuRegister_8 register_ = beemu_get_register_from_letter_8(register_name[0]);
-			beemu_registers_write_8(processor->registers, register_, (uint8_t)strtol(val, NULL, 0));
+			beemu_registers_write_8(processor->registers, register_, (uint8_t)strtol(value, NULL, 0));
 		}
 	}
 	const int number_of_ram_addresses = json_object_array_length(initial_ram_values);
@@ -122,18 +147,21 @@ void exec_test_case(json_object *test)
 	json_object_object_foreach(final_cpu_values, key2, val2)
 	{
 		const char *register_name = key2;
+		const char *expected_str = json_object_get_string(val2);
 		if (strncmp(register_name, "sp", 2) == 0 || strncmp(register_name, "pc", 2) == 0)
 		{
 			const BeemuRegister_16 register_ = strncmp(register_name, "sp", 2) == 0 ? BEEMU_REGISTER_SP : BEEMU_REGISTER_PC;
 			const uint16_t value = beemu_registers_read_16(processor->registers, register_);
-			TEST_ASSERT_EQUAL((uint16_t)strtol(val2, NULL, 0), value);
+			const uint16_t expected = (uint16_t)strtol(expected_str, NULL, 0);
+			TEST_ASSERT_EQUAL_MESSAGEF(expected, value, "In Test %s, Expected %x but got %x in Register %s", test_name, expected, value, register_name);
 		}
 		else
 		{
 			// 8 bit registers
 			const BeemuRegister_8 register_ = beemu_get_register_from_letter_8(register_name[0]);
 			const uint8_t value = beemu_registers_read_8(processor->registers, register_);
-			TEST_ASSERT_EQUAL((uint8_t)strtol(val2, NULL, 0), value);
+			const uint8_t expected = (uint8_t)strtol(expected_str, NULL, 0);
+			TEST_ASSERT_EQUAL_MESSAGEF(expected, value, "In Test %s, Expected %x but got %x in Register %s", test_name, expected, value, register_name);
 		}
 	}
 	const int number_of_ram_addresses_in_final = json_object_array_length(initial_ram_values);
@@ -143,7 +171,7 @@ void exec_test_case(json_object *test)
 		const int addr = (uint16_t)strtol(json_object_get_string(json_object_array_get_idx(ram_addr_val, 0)), NULL, 0);
 		const int val = (uint8_t)strtol(json_object_get_string(json_object_array_get_idx(ram_addr_val, 1)), NULL, 0);
 		const uint8_t actual_value = beemu_memory_read(processor->memory, addr);
-		TEST_ASSERT_EQUAL(val, actual_value);
+		TEST_ASSERT_EQUAL_MESSAGEF(val, actual_value, "In Test %s, Expected %x but got %x in Memory Location %x", test_name, val, actual_value, addr);
 	}
 	beemu_processor_free(processor);
 }
@@ -155,6 +183,7 @@ void exec_test_case(json_object *test)
  */
 void exec_single_instruction(char *data_file)
 {
+	TEST_MESSAGEF("Starting Test %s.", data_file);
 	json_object *root = json_object_from_file(data_file);
 	if (!root)
 	{
@@ -170,9 +199,7 @@ void exec_single_instruction(char *data_file)
 		json_object *test = json_object_array_get_idx(root, test_id);
 		exec_test_case(test);
 	}
-	char msg[100];
-	snprintf(msg, 100, "Test %s Passed.");
-	TEST_MESSAGE(msg);
+	TEST_MESSAGEF("Test %s Passed.", data_file);
 	json_object_put(root);
 }
 
