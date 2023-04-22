9a49f06e3603646a186653664152ce75e1ce5d61
ambertide <egeemirozkan24@gmail.com>
2023-04-22 17:10:14 +0300

    tests(registers): add flag getter setter tests

--- tests/include/BeemuTest.hpp
+++ tests/include/BeemuTest.hpp
@@ -9,8 +9,13 @@ namespace BeemuTests
 	protected:
 		BeemuMemory *memory;
 		BeemuRegisters *registers;
-		BeemuRegister hl_register = {.type = BEEMU_SIXTEEN_BIT_REGISTER, .name_of.sixteen_bit_register = BEEMU_REGISTER_HL};
-
+		BeemuRegister hl = {.type = BEEMU_SIXTEEN_BIT_REGISTER, .name_of.sixteen_bit_register = BEEMU_REGISTER_HL};
+		BeemuRegister a = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_A};
+		BeemuRegister b = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_B};
+		BeemuRegister c = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_C};
+		BeemuRegister d = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_D};
+		BeemuRegister h = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_H};
+		BeemuRegister l = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_L};
 		void SetUp() override;
 
 		void TearDown() override;
--- tests/test_registers.cpp
+++ tests/test_registers.cpp
@@ -6,16 +6,15 @@ namespace BeemuTests
 {
 	TEST_F(BeemuTestFixture, RegistersWriteReadTest8)
 	{
-		BeemuRegister register_ = {.type = BEEMU_EIGHT_BIT_REGISTER, .name_of.eight_bit_register = BEEMU_REGISTER_A};
-		beemu_registers_write_register_value(registers, register_, 8);
-		const uint16_t value = beemu_registers_read_register_value(registers, register_);
+		beemu_registers_write_register_value(registers, a, 8);
+		const uint16_t value = beemu_registers_read_register_value(registers, a);
 		EXPECT_EQ(value, 8);
 		EXPECT_EQ(registers->registers[BEEMU_REGISTER_A], 8);
 	}
 
 	TEST_F(BeemuTestFixture, RegistersWriteTest16)
 	{
-		beemu_registers_write_register_value(registers, hl_register, 0xAAFF);
+		beemu_registers_write_register_value(registers, hl, 0xAAFF);
 		EXPECT_EQ(registers->registers[BEEMU_REGISTER_H], 0xAA);
 		EXPECT_EQ(registers->registers[BEEMU_REGISTER_L], 0xFF);
 	}
@@ -24,12 +23,32 @@ namespace BeemuTests
 	{
 		registers->registers[BEEMU_REGISTER_H] = 0xAA;
 		registers->registers[BEEMU_REGISTER_L] = 0xFF;
-		EXPECT_EQ(beemu_registers_read_register_value(registers, hl_register), 0xAAFF);
+		EXPECT_EQ(beemu_registers_read_register_value(registers, hl), 0xAAFF);
 	}
 
 	TEST_F(BeemuTestFixture, RegisterReadWriteTest16)
 	{
-		beemu_registers_write_register_value(registers, hl_register, 0xAAFF);
-		EXPECT_EQ(beemu_registers_read_register_value(registers, hl_register), 0xAAFF);
+		beemu_registers_write_register_value(registers, hl, 0xAAFF);
+		EXPECT_EQ(beemu_registers_read_register_value(registers, hl), 0xAAFF);
+	}
+
+	TEST_F(BeemuTestFixture, FlagSetTest)
+	{
+		beemu_registers_flags_set_flag(registers, BEEMU_FLAG_Z, 1);
+		EXPECT_EQ(registers->flags, 0b10000000);
+	}
+
+	TEST_F(BeemuTestFixture, FlagGetTest)
+	{
+		registers->flags = 0b10000000;
+		uint8_t z_flag = beemu_registers_flags_get_flag(registers, BEEMU_FLAG_Z);
+		EXPECT_EQ(z_flag, 1);
+	}
+
+	TEST_F(BeemuTestFixture, FlagGetSetTest)
+	{
+		beemu_registers_flags_set_flag(registers, BEEMU_FLAG_Z, 1);
+		uint8_t z_flag = beemu_registers_flags_get_flag(registers, BEEMU_FLAG_Z);
+		EXPECT_EQ(z_flag, 1);
 	}
 }
