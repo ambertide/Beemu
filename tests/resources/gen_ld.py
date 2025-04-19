7424df1454b4229a1af856734a6075507af30949
Ege Özkan <egeemirozkan24@gmail.com>
2025-04-19 19:25:12 +0300

    test(tokenizer): add load indirect 16 tests

--- tests/resources/gen_ld.py
+++ tests/resources/gen_ld.py
@@ -14,13 +14,21 @@ for token in tokens:
     opcode = get_opcode(token["token"]["original_machine_code"])
 
     if (0x7F >= opcode >= 0x40) or opcode in [
+        0x02,
         0x06,
+        0x0A,
         0x0E,
+        0x12,
         0x16,
+        0x1A,
         0x1E,
+        0x22,
         0x26,
+        0x2A,
         0x2E,
+        0x32,
         0x36,
+        0x3A,
         0x3E,
     ]:
         to_be_removed.append(token["token"]["original_machine_code"])
@@ -158,6 +166,62 @@ for lsb in range(4):
 
         tokens.append(instruction)
         register_index += 1
+
+# Now finally let us build the final block, this block goes is those instructions in
+# 0xX2 and 0x0A, and what loads indirectly form 16-bit registers or loads TO them from A
+
+
+A_REGISTER = {
+    "pointer": False,
+    "type": "BEEMU_PARAM_TYPE_REGISTER_8",
+    "value": f"BEEMU_REGISTER_A",
+}
+
+registers = ["BC", "DE", "HL", "HL"]
+ops = [
+    "BEEMU_POST_LOAD_NOP",
+    "BEEMU_POST_LOAD_NOP",
+    "BEEMU_POST_LOAD_INCREMENT_INDIRECT_DESTINATION",
+    "BEEMU_POST_LOAD_DECREMENT_INDIRECT_DESTINATION",
+]
+
+# Stands for least significant nibble btw.
+for lsn in range(4):
+    reg_16 = registers[lsn]
+    register = {
+        "pointer": True,
+        "type": "BEEMU_PARAM_TYPE_REGISTER_16",
+        "value": {"register_16": f"BEEMU_REGISTER_{reg_16}"},
+    }
+
+    # in 0xX2 it starts as R16 <- A
+    dest = register
+    src = A_REGISTER
+    post_op = ops[lsn]
+    for msn in [0x02, 0xA]:
+        instruction = (lsn << 4) | msn
+        token = {
+            "instruction": f"0x{instruction:06X}",
+            "token": {
+                "type": "BEEMU_INSTRUCTION_TYPE_LOAD_8",
+                "duration_in_clock_cycles": 2,
+                "original_machine_code": instruction,
+                "byte_length": 1,
+                "params": {
+                    "load_params": {
+                        "source": src,
+                        "dest": dest,
+                        # Fun story...
+                        "postLoadOperation": post_op,
+                    }
+                },
+            },
+        }
+        # But we swap in 0xXA!
+        dest, src = src, dest
+        # the operation as well.
+        post_op = post_op.replace("DESTINATION", "SOURCE")
+        tokens.append(token)
 
 sort_instructions(tokens)
 
