6483c866da98e22ebf8dfa5b3e097832453ab0e4
Ege Özkan <egeemirozkan24@gmail.com>
2025-05-04 12:02:53 +0300

    feat(tokenizer): add inc/dec instructions

--- src/libbeemu/device/processor/tokenizer/tokenize_arithmatic8.c
+++ src/libbeemu/device/processor/tokenizer/tokenize_arithmatic8.c
@@ -11,19 +11,20 @@ arithmatic8_subtype_if_arithmatic8(uint8_t opcode)
 		{ 0xFF, 0xFF },
 		// Mainline check specifically looks at whether or not
 		// first nibble is 0x8, 0x9, 0xA or 0xB.
-		{ 0xC0, 0x80 }
+		{ 0xC0, 0x80 },
+		{ 0xC6, 0x04 }
 	};
 
 	return instruction_subtype_if_of_instruction_type(
 	    opcode,
 	    tests,
 	    BEEMU_TOKENIZER_ARITHMATIC8_INVALID_ARITHMATIC8,
-	    BEEMU_TOKENIZER_ARITHMATIC8_MAINLINE_ARITHMATIC8);
+	    BEEMU_TOKENIZER_ARITHMATIC8_INC_DEC);
 }
 
 void determine_mainline_arithmatic8_params(
     BeemuInstruction* instruction,
-    uint8_t opcode)
+    const uint8_t opcode)
 {
 	static const BeemuOperation suboperations[] = {
 		BEEMU_OP_ADD,
@@ -53,25 +54,54 @@ void determine_mainline_arithmatic8_params(
 	    7);
 }
 
+void determine_arithmatic8_inc_dec_params(
+    BeemuInstruction* instruction,
+    const uint8_t opcode)
+{
+	static const BeemuOperation suboperations[] = {
+		// A rather... interseting design decision I had given early on
+		// was to encode INC and DEC as add and sub respectively.
+		BEEMU_OP_ADD,
+		BEEMU_OP_SUB
+	};
+	const uint8_t operation_selector = opcode & 0x1;
+	const uint8_t register_selector = (opcode & 0b00111000) >> 3;
+	tokenize_register_param_with_index(&instruction->params.arithmatic_params.dest_or_first, register_selector);
+	static const BeemuParam incrementParam = {
+		.pointer = false,
+		.type = BEEMU_PARAM_TYPE_UINT_8,
+		.value.value = 1
+	};
+	instruction->params.arithmatic_params.source_or_second = incrementParam;
+	instruction->params.arithmatic_params.operation = suboperations[operation_selector];
+}
+
 // Array used to dispatch to the determine_load8_SUBTYPE_params function
 // for a specific BEEMU_TOKENIZER_LOAD8_SUBTYPE, parallel array to the enum
 // values
-static const determine_param_function_ptr DETERMINE_PARAM_DISPATCH[] = {
-	0,
-	&determine_mainline_arithmatic8_params
-};
+static const determine_param_function_ptr DETERMINE_PARAM_DISPATCH[]
+    = {
+	      0,
+	      &determine_mainline_arithmatic8_params,
+	      &determine_arithmatic8_inc_dec_params
+      };
 
-void determine_arithmatic8_params(BeemuInstruction* instruction, uint8_t opcode, BEEMU_TOKENIZER_ARITHMATIC8_SUBTYPE load_subtype)
+void determine_arithmatic8_params(BeemuInstruction* instruction, uint8_t opcode, BEEMU_TOKENIZER_ARITHMATIC8_SUBTYPE arithmatic8_params)
 {
-	determine_param_function_ptr determine_params_func = DETERMINE_PARAM_DISPATCH[load_subtype];
+	determine_param_function_ptr determine_params_func = DETERMINE_PARAM_DISPATCH[arithmatic8_params];
 	determine_params_func(instruction, opcode);
 }
 
-void determine_arithmatic8_clock_cycle(BeemuInstruction* instruction)
+void determine_arithmatic8_clock_cycle(BeemuInstruction* instruction, BEEMU_TOKENIZER_ARITHMATIC8_SUBTYPE operation_subtype)
 {
 	instruction->duration_in_clock_cycles = 1;
 	if (instruction->params.arithmatic_params.source_or_second.pointer) {
 		instruction->duration_in_clock_cycles++;
+	}
+	if (instruction->params.arithmatic_params.dest_or_first.pointer && operation_subtype == BEEMU_TOKENIZER_ARITHMATIC8_INC_DEC) {
+		// For INC/DEC a pointer causes a read AND a write.
+		// so, extra time spent.
+		instruction->duration_in_clock_cycles = 3;
 	}
 }
 
@@ -81,6 +111,5 @@ void tokenize_arithmatic8(BeemuInstruction* instruction, uint8_t opcode)
 	assert(arithmatic8_subtype != BEEMU_TOKENIZER_ARITHMATIC8_INVALID_ARITHMATIC8);
 	instruction->type = BEEMU_INSTRUCTION_TYPE_ARITHMATIC_8;
 	determine_arithmatic8_params(instruction, opcode, arithmatic8_subtype);
-	determine_arithmatic8_clock_cycle(instruction);
-	return;
+	determine_arithmatic8_clock_cycle(instruction, arithmatic8_subtype);
 }
