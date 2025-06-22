82a1b22137241fd9223e679e73563f8c53c79b8d
Ege Özkan <egeemirozkan24@gmail.com>
2025-06-22 17:38:05 +0300

    test(tokenizer): add rst tests

---
+++ tests/resources/gen_jump.py
@@ -0,0 +1,43 @@
+from json import dump
+
+from tests.resources.utils import get_tokens_except, sort_instructions
+
+rst_instructions = [*range(0xC7, 0x100, 8)]
+
+tokens = get_tokens_except([*rst_instructions])
+
+# RST INSTRUCTIONS START
+mem_addresses = range(0x00, 0x39, 8)
+
+for opcode, mem_address in zip(rst_instructions, mem_addresses):
+    tokens.append({
+        "instruction": f"{opcode:06X}",
+        "token": {
+            "type": "BEEMU_INSTRUCTION_TYPE_ARITHMATIC",
+            "duration_in_clock_cycles": 4,
+            "original_machine_code": opcode,
+            "byte_length": 1,
+            "params": {
+                "jump_params": {
+                    "is_conditional": False,
+                    "is_relative": False,
+                    "enable_interrupts": False,
+                    "type": "BEEMU_JUMP_TYPE_RST",
+                    "condition": "BEEMU_JUMP_IF_NO_CONDITION",
+                    "param": {
+                       "pointer": True,
+                        "type": "BEEMU_PARAM_TYPE_UINT16",
+                        "value": { "value": mem_address }
+                    }
+                }
+            },
+        }
+    })
+
+
+if __name__ == '__main__':
+    sort_instructions(tokens)
+    with open("tokens.json", "w") as file:
+        dump({"tokens": tokens}, file, indent="\t")
+
+    print(f"Inserted all calls, reaching a total of {len(tokens)}.")
