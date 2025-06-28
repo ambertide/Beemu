d7a29b2e790a362ace87ec733ec0c83d2714bce1
Ege Özkan <egeemirozkan24@gmail.com>
2025-06-28 15:44:04 +0300

    test(tokenizer): add jump relative tests

--- tests/resources/gen_jump.py
+++ tests/resources/gen_jump.py
@@ -2,14 +2,78 @@ from json import dump
 
 from tests.resources.utils import get_tokens_except, sort_instructions
 
+jr_conditional = [*range(0x20, 0x39, 8)]
 rst_instructions = [*range(0xC7, 0x100, 8)]
 conditional_jp_instructions = [*range(0xC2, 0xDB, 8)]
 
 tokens = get_tokens_except([
+    0x18,
+    *jr_conditional,
     *rst_instructions,
     *conditional_jp_instructions,
     0xC3
 ])
+
+conditions = [
+    "BEEMU_JUMP_IF_NOT_ZERO",
+    "BEEMU_JUMP_IF_ZERO",
+    "BEEMU_JUMP_IF_NOT_CARRY",
+    "BEEMU_JUMP_IF_CARRY"
+]
+
+
+# JR INSTRUCTIONS START
+
+
+# first unconditional
+tokens.append({
+    "instruction": f"0x0018F0",
+    "token": {
+        "type": "BEEMU_INSTRUCTION_TYPE_JUMP",
+        "duration_in_clock_cycles": 3,
+        "original_machine_code": (0x18 << 8) + 0xCD,
+        "byte_length": 2,
+        "params": {
+            "jump_params": {
+                "is_conditional": False,
+                "is_relative": True,
+                "enable_interrupts": False,
+                "type": "BEEMU_JUMP_TYPE_JUMP",
+                "condition": "BEEMU_JUMP_IF_NO_CONDITION",
+                "param": {
+                    "pointer": True,
+                    "type": "BEEMU_PARAM_TYPE_INT_8",
+                    "value": { "signed_value": 0xF0 }
+                }
+            }
+        },
+    }
+})
+# then conditional
+for opcode, condition in zip(jr_conditional, conditions):
+    tokens.append({
+        "instruction": f"0x00{opcode:02X}F0",
+        "token": {
+            "type": "BEEMU_INSTRUCTION_TYPE_JUMP",
+            "duration_in_clock_cycles": 3,
+            "original_machine_code": (opcode << 8) + 0xCD,
+            "byte_length": 2,
+            "params": {
+                "jump_params": {
+                    "is_conditional": True,
+                    "is_relative": True,
+                    "enable_interrupts": False,
+                    "type": "BEEMU_JUMP_TYPE_JUMP",
+                    "condition": condition,
+                    "param": {
+                        "pointer": True,
+                        "type": "BEEMU_PARAM_TYPE_INT_8",
+                        "value": { "signed_value": 0xF0 }
+                    }
+                }
+            },
+        }
+    })
 
 # RST INSTRUCTIONS START
 mem_addresses = range(0x00, 0x39, 8)
@@ -38,13 +102,6 @@ for opcode, mem_address in zip(rst_instructions, mem_addresses):
             },
         }
     })
-
-conditions = [
-    "BEEMU_JUMP_IF_NOT_ZERO",
-    "BEEMU_JUMP_IF_ZERO",
-    "BEEMU_JUMP_IF_NOT_CARRY",
-    "BEEMU_JUMP_IF_CARRY"
-]
 
 for opcode, condition in zip(conditional_jp_instructions, conditions):
     tokens.append({
