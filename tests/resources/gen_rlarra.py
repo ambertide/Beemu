96a8837422c37babfcc3b0f65fd289cfaa6ed4bf
Ege Özkan <egeemirozkan24@gmail.com>
2025-06-29 12:54:32 +0300

    test(tokenizer): add rrca, rlca, rla, rra tests

--- tests/resources/gen_rlarra.py
+++ tests/resources/gen_rlarra.py
@@ -12,6 +12,31 @@ for token in tokens:
     if token['token']['type'] == 'BEEMU_INSTRUCTION_TYPE_ROT_SHIFT':
         token['token']['params']['rot_shift_params']['set_flags_to_zero'] = False
 
+for opcode in insts:
+    tokens.append({
+        "instruction": f"0x{opcode:06X}",
+        "token": {
+            "type": "BEEMU_INSTRUCTION_TYPE_ROT_SHIFT",
+            "duration_in_clock_cycles": 1,
+            "original_machine_code": opcode,
+            "byte_length": 1,
+            "params": {
+                "rot_shift_params": {
+                    "through_carry": opcode < 0x10,
+                    "operation": "BEEMU_ROTATE_OP",
+                    "direction": "BEMU_LEFT_DIRECTION" if opcode in [0x07, 0x17]  else "BEEMU_RIGHT_DIRECTION",
+                    "target": {
+                        "pointer": False,
+                        "type": "BEEMU_PARAM_TYPE_REGISTER_8",
+                        "value": {
+                            "register_8": "BEEMU_REGISTER_A"
+                        }
+                    },
+                    "set_flags_to_zero": True
+                }
+            }
+        }
+    })
 
 if __name__ == '__main__':
     sort_instructions(tokens)
