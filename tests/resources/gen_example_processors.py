1b077495ac728fac261e882c67c74a63c10930be
Ege Özkan <egeemirozkan24@gmail.com>
2025-07-06 19:04:30 +0300

    chore(test): start adding example machine states

---
+++ tests/resources/gen_example_processors.py
@@ -0,0 +1,40 @@
+# Generate the example machine states to be used in testing.
+
+from itertools import cycle, islice
+from json import dump
+
+machine_states = {
+    "default": {
+        "registers": {
+            # A, B, C, D, E, H and L respectively.
+            "registers": [0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x01, 0x02],
+            # Flags, the first four bits are useless, if I am able to
+            # read what I wrote.
+            "flags": 0b00001111,
+            "stack_pointer": 0xFF,
+            "program_counter": 0x00
+        },
+        "memory": {
+            "memory_size": 2*16 - 1,
+            # Fill with numbers repeating from 0x00 to 0xFF until addressable space (0xFFFF) is over.
+            "memory": [mem_value for mem_value in islice(cycle(range(0x00, 0xFF + 1)), 0xFFFF + 1)]
+        },
+        "processor_state": "BEEMU_DEVICE_NORMAL",
+        "interrupts_enabled": True,
+        "elapsed_clock_cycle": 0
+    }
+}
+
+if __name__ == '__main__':
+    output = {
+        "machine_states": []
+    }
+
+    for machine_state_name, machine_state in machine_states.items():
+        output['machine_states'].append({
+            "preset": machine_state_name,
+            "processor": machine_state
+        })
+
+    with open('processors.json', 'w') as file:
+        dump(output, file)
