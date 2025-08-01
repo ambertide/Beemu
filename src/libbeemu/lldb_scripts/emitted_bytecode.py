825bf365a13e17bf0d12b531f3214532b8a9ca65
Ege Özkan <egeemirozkan24@gmail.com>
2025-08-01 22:50:53 +0300

    feat(lldb_scripts): add expected command as well

--- src/libbeemu/lldb_scripts/emitted_bytecode.py
+++ src/libbeemu/lldb_scripts/emitted_bytecode.py
@@ -2,6 +2,9 @@
 Print out the emitted bytecodes for a specific instruction
 by the `beemu_parser_parse` function using `lldb`
 """
+from json import load
+from plistlib import loads
+from typing import Generator
 
 import lldb
 import os
@@ -26,7 +29,24 @@ value_map = {
     'false': 'byte_value'
 }
 
-def parse_node(instr: lldb.SBValue) -> None:
+def parse_expected_node() -> Generator[str, None, None]:
+    with open('tests/resources/command_tests.json') as f:
+        all_instrs: list = load(f)['commands']
+    instr = next(inst for inst in all_instrs if inst['token']['original_machine_code'] == int(opcode, base=16))
+    commands = instr['command_queue']
+    for command in commands:
+        if command['type'] == 'BEEMU_COMMAND_WRITE':
+            command_target = [*command['write']['target']['target'].values()][0]
+            command_values = [*command['write']['value']['value'].values()][0]
+            yield f'WRITE {command_target} {command_values}'
+        elif command['type'] == 'BEEMU_COMMAND_HALT':
+            yield 'HALT'
+        else:
+            yield 'UNSUPPORTED'
+
+
+
+def parse_node(instr: lldb.SBValue) -> str:
     type_: SBValue = instr.GetChildMemberWithName('type').GetValue()
     if type_ == 'BEEMU_COMMAND_WRITE':
         write_info = instr.GetChildMemberWithName('write')
@@ -38,14 +58,17 @@ def parse_node(instr: lldb.SBValue) -> None:
         write_value_type = write_value_info.GetChildMemberWithName('is_16').GetValue()
         write_value_field = value_map.get(write_value_type)
         write_value = write_value_info.GetChildMemberWithName('value').GetChildMemberWithName(write_value_field).GetValue()
-        print(f'WRITE {write_target} {write_value}')
+        return f'WRITE {write_target} {write_value}'
     elif type_ == 'BEEMU_COMMAND_HALT':
-        print('HALT')
+        return 'HALT'
     else:
-        print(instr)
-        print('UNSUPPORTED')
+        return 'UNSUPPORTED'
 
 
+def print_on_centre(ls: str, rs: str) -> None:
+    left_str_len = len(ls)
+    right_str_len = len(rs)
+    print(f"{ls}{' ' * (80 - left_str_len)}{rs}")
 
 if __name__ == '__main__':
     # Set the path to the executable to debug
@@ -94,8 +117,12 @@ if __name__ == '__main__':
                 # attempt to find the first member.
                 child: lldb.SBValue = commands.GetChildMemberWithName('first')
                 last = commands.GetChildMemberWithName('last')
+                expected_generator = parse_expected_node()
+                print_on_centre('Actual Command', 'Expected Command')
                 while child.GetChildMemberWithName('next').GetValue() != last.GetChildMemberWithName('next').GetValue():
-                    parse_node(child.GetChildMemberWithName('current'))
+                    actual_node = parse_node(child.GetChildMemberWithName('current'))
+                    expected_node = next(expected_generator)
+                    print_on_centre(actual_node, expected_node)
                     child = child.GetChildMemberWithName('next')
                 else:
                     parse_node(child.GetChildMemberWithName('current'))
